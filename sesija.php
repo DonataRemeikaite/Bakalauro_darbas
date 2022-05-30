2951ade4d83ddb86bbc0ecc7b6fdd435106f9717
DonataRemeikaite <106170975+DonataRemeikaite@users.noreply.github.com>
2022-05-30 22:40:10 +0300

    Update sesija.php

--- sesija.php
+++ sesija.php
@@ -3,7 +3,7 @@ session_start();
 
 $servername = "localhost";
 $username = "if180029";
-$password = "yb*EGYVcWzZL";
+
 $dbname = "if180029";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
